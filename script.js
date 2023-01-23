b201d5f219bf309b05ce2f46872266b9a8bb1100
Anurag Rawat <117522825+AnuRAW@users.noreply.github.com>
2023-01-23 12:57:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function threeSum(arr, target) {
   //your code here
+	const express = require("express");
+const path = require("path");
+const threeSum = require("./script");
+const app = express();
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/main.html"));
+});
+
+app.post("/threesum", (req, res) => {
+  const { arr, target } = req.body;
+  const ans = threeSum(arr, target);
+  res.send({ message: ans });
+});
+
+module.exports = app;
 }
 
 module.exports = threeSum;
